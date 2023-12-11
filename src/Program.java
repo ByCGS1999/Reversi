7b3b3ffc031048b7105db0e0a229536a5f2ed4a3
CGS <cgs@cgs.com>
2023-12-11 16:26:25 +0100

    new structure

---
+++ src/Program.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Program
+{
+    static char[][] board;
+
+    static void SetBoardValue(int x, int y, char c)
+    {
+        board[x][y] = c;
+    }
+
+    static void Init()
+    {
+        board = new char[8][8];
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        Init();
+
+        s.close();
+    }
+}
