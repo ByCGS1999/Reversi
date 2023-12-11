09d24d9c2b7242d46feffd9d55f3d51c79178a74
CGS <cgs@cgs.com>
2023-12-11 16:24:05 +0100

    Added board values

--- Program.java
+++ Program.java
@@ -2,9 +2,16 @@ import java.util.*;
 
 public class Program
 {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        s.close();
+    static char[][] board;
+
+    static void SetBoardValue(int x, int y, char c)
+    {
+        board[x][y] = c;
     }
 
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        s.close();
+    }
 }
