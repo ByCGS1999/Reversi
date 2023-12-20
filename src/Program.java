78589368bee45f9633c7af270e4d500434384816
FE4Rv <victorpazmeseguer@gmail.com>
2023-12-20 16:52:08 +0100

    RTXDisplay

--- src/Program.java
+++ src/Program.java
@@ -237,6 +237,15 @@ public class Program {
     }
 
     static void menu() {
+        System.out.println(" _______ _________            _______  _     ");
+        System.out.println("(  _  _ )\__   __/|\     /|  (  ___  )( (    /|");
+        System.out.println("| (    )|   ) (   ( \   / )  | (   ) ||  \  ( |");
+        System.out.println("| (____)|   | |    \ (_) /   | |   | ||   \ | |");
+        System.out.println("|     __)   | |     ) _ (    | |   | || (\ \) |");
+        System.out.println("| (\ (      | |    / ( ) \   | |   | || | \   |");
+        System.out.println("| ) \ \__   | |   ( /   \ )  | (___) || )  \  |");
+        System.out.println("|/   \__/   )_(   |/     \|  (_______)|/    )_)");
+
         System.out.println("+----------------------------------+");
         System.out.println("| 1.Play                           |");
         System.out.println("| 2.Exit                           |");
